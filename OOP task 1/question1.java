c5839c6e1b0bbf91265e12c35c373cc1b9755ba2
Shahzaneer Ahmed <shahism01@gmail.com>
2022-02-15 16:07:37 +0500

    "lab task 1 added"

--- OOP task 1/question1.java
+++ OOP task 1/question1.java
@@ -2,19 +2,19 @@
 public class question1{
     public static void main(String [] args){
     int a = 2; 
-    int b = 2;
+    int b = 5;
     int power = power(a,b);
     System.out.println(power);
 
     }
     static int power(int num1,int num2){
-        int div = num1*num2;
+        int pow = 1;
         
         for(int i =1;i<=num2;i++){
-        num1*=num1;
+        pow*=num1;
         }
     
-        return num1;
+        return pow;
         
     }
 }
