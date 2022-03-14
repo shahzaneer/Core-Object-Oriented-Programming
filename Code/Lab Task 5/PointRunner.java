4d175da1ae73b8d00da9ce23299b19640cfac16e
Shahzaneer Ahmed <shahism01@gmail.com>
2022-03-14 10:55:35 +0500

    Account point rectangle class added lab 5

--- Code/Lab Task 5/PointRunner.java
+++ Code/Lab Task 5/PointRunner.java
@@ -3,6 +3,8 @@ public class PointRunner {
         Point p1 = new Point(2,5);
         Point p2 = new Point(3,7);
         Point p3 = new Point(2,2);
+
+        // this is basically pointer for the object!
         Point a = p3.addThreepoints(p1, p2);
 
         System.out.println(a.getX());
