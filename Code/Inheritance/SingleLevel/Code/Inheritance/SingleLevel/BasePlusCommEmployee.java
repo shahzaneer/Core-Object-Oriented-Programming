a2952475201ba872914b84a5bb5710f6e3165732
Shahzaneer Ahmed <shahism01@gmail.com>
2022-04-04 16:22:12 +0500

    Single level inheritance Employee example added

---
+++ Code/Inheritance/SingleLevel/Code/Inheritance/SingleLevel/BasePlusCommEmployee.java
@@ -0,0 +1,17 @@
+package Code.Inheritance.SingleLevel;
+public class BasePlusCommEmployee extends CommEmployee{
+    private double basicSalary;
+
+    public BasePlusCommEmployee(){
+        super();
+    }
+    public BasePlusCommEmployee(String firstName , String lastName,double grossSale,double commRate,double basicSalary){
+        super(firstName, lastName, grossSale, commRate);
+        this.basicSalary = basicSalary;
+    }
+
+    public void display(){
+        super.display();
+        System.out.println(this.basicSalary);
+    }
+}
---
+++ Code/Inheritance/SingleLevel/Code/Inheritance/SingleLevel/Runner.java
@@ -0,0 +1,12 @@
+package Code.Inheritance.SingleLevel;
+
+import Code.Inheritance.SingleLevel.BasePlusCommEmployee;
+// package Code.Inheritance.SingleLevel;
+
+
+public class Runner {
+    public static void main(String[] args) {
+        Code.Inheritance.SingleLevel.BasePlusCommEmployee emp1 = new Code.Inheritance.SingleLevel.BasePlusCommEmployee("Shahzaneer","Ahmed",450.0,300,9000);
+        emp1.display();
+    }
+}
