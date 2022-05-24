8fe9df9b099af81c8c1362ca688c47d6fd3640c6
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-05-24 23:52:48 +0500

    autoboxing unboxing boxing added

--- Code/ArrayList/AutoBoxing and Unboxing/Lastly.java
+++ Code/ArrayList/AutoBoxing and Unboxing/Lastly.java
@@ -3,6 +3,20 @@ import java.util.ArrayList;
 // 3.	Create an array List of any wrapper class.
 // Add 5 objects and explore the concept of autoboxing and unboxing
 
+
+        //! Unboxing
+
+        // Converting an object of a wrapper type (Integer) to its 
+        // corresponding primitive (int) value is called unboxing. 
+
+        // !Autoboxing
+
+        // Autoboxing is the automatic conversion that 
+        // the Java compiler makes between the primitive types
+        // and their corresponding object wrapper classes. 
+        // For example, converting an int to an Integer, a double to a Double, and so on.
+
+
 public class Lastly {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
@@ -10,16 +24,28 @@ public class Lastly {
 
         //Dynamic part main <> generic specifically mention karen na karen koi masla nhi hota
         // * generics compile time per resolve houtay hain
-        
+
         names.add("Shahzaneer Ahmed");
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i));
         }
-
+        // normal way which should be used
+        // numbers.add(new Integer(6));
+//      auto boxing
         numbers.add(96);
         for (int i = 0; i < numbers.size(); i++) {
             System.out.println(numbers.get(i));
+            System.out.println(numbers.get(i).getClass().getSimpleName());
         }
+
+
+//     This is user defined boxing
+        int a = 56;
+        System.out.println(((Object)a).getClass().getSimpleName());
+
+
+
+
 
     }
 }
